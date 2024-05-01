1b3b8905363784f2d5e89803607a8443f1c8814e
bryzgalova <eibryzgalova@mai.education>
2024-05-01 21:34:23 +0300

    готовы все слайды, кроме 4 и 6

--- layouts/main_layout.py
+++ layouts/main_layout.py
@@ -1,9 +1,11 @@
 from dash import html
 import dash_bootstrap_components as dbc
+from components.navbar import create_navbar
 
 
 def create_main_layout():
     layout = html.Div([
+       # create_navbar(),
         html.Div([
             html.Div([
                 html.H1('Будь в Курсе', className='main-layout-header-text', style={
@@ -15,12 +17,17 @@ def create_main_layout():
                     'color': 'white',
                 }),
                 html.Div([
-                    html.P('место, где собраны все новости, изменившие курс рубля не только сегодня',
-                           className='main-layout-text'),
+                    html.P(['место, где собраны все новости,',
+                                    html.Br(),  # Перенос строки
+                                    'изменившие курс рубля не только сегодня'],
+                           className='main-layout-text', style={'font-size': '2vw'}),
                     html.A(html.Button([
-                        html.Span('Следить', className='follow-text'),
-                        html.Span(' за курсом', className='currency-text')
-                    ], id='follow-currency-button', className='follow-currency-button'), href='/currency-analysis'),
+                        html.Span('Следить за курсом', className='follow-text'),
+                    ], id='follow-currency-button', className='follow-currency-button',
+                        style={'height': '3vw', 'width': '15vw', 'display': 'flex', 'align-items': 'center',
+                               'justify-content': 'center',
+                               'margin-left': 'auto', 'margin-right': 'auto'}),
+                        href='/currency-analysis', style={'text-decoration': 'none'}),
                 ]),
             ], style={
                 'position': 'absolute',
@@ -31,13 +38,65 @@ def create_main_layout():
                 'color': 'white',
             }),
         ], style={
-            'background-image': 'url(/assets/main_jpeg.jpg)',
+            'background-image': 'url(/assets/main-dark.png)',
             'height': '90vh',
             'background-size': 'cover',
             'background-position': 'center center',
             'position': 'relative',
+            #'z-index': 900,
+            #'margin-top': '120px',  # Учесть высоту навбара
         }),
-        html.Div([ "Второй фон"
+#2 слайд
+        html.Div([
+                   html.H1('О нас', className='main-layout-header-text-2', style={
+                       'position': 'absolute',
+                       'top': '25%',
+                       'left': '8%',
+                   }),
+                    html.P([
+                        'Обычные студенты, воплощающие свои идеи',
+                        html.Br(),
+                        'в жизнь!'],
+                        className='main-layout-text', style={
+                       'position': 'absolute',
+                       'top': '35%',
+                       'left': '8%'}),
+            html.P([
+                'Мы создали место, где каждый может',
+                html.Br(),
+                'отслеживать курс рубля и узнавать, как самые',
+                html.Br(),
+                'актуальные новости влияют на валюту'],
+                className='main-layout-text', style={
+                    'font-weight': 'bold',
+                    'position': 'absolute',
+                    'top': '50%',
+                    'left': '6.5%',
+                    'padding-left': '20px',  # Добавляем отступ слева для декоративной полоски
+                    'border-left': '4px solid #30D9CA',
+                }),
+            html.A(html.Button([
+                html.Span('Зачем смотреть курс рубля', className='follow-text'),
+            ], id='why-follow-currency-button', className='follow-currency-button',
+                style={'height': '3.3vw', 'width': '21vw', 'display': 'flex', 'align-items': 'center',
+                       'justify-content': 'center',
+                       'position': 'absolute',
+                       'top': '62%',
+                       'left': '13%'
+                       }),
+                href='/currency-analysis', style={'text-decoration': 'none'}),
+    html.Div([
+        html.Div([
+            html.Img(src='/assets/image1.jpg', style={'width': '16vw', 'height': '16vw', 'margin-right': '20px'}),
+            html.Img(src='/assets/image2.png', style={'width': '16vw', 'height': '16vw', 'margin-top': '40px'}),
+        ], style={'display': 'flex'}),
+        html.Div([
+            html.Img(src='/assets/image3.png', style={'width': '16vw', 'height': '16vw', 'margin-right': '20px'}),
+            html.Img(src='/assets/image4.jpg', style={'width': '16vw', 'height': '16vw', 'margin-top': '40px'}),
+        ], style={'display': 'flex'}),
+    ], style={'position': 'absolute', 'top': '7%', 'right': '7%'}),
+
+
         ], style={
             'background-color': 'rgba(217, 217, 217, 0.26)',
             'height': '90vh',
@@ -45,15 +104,102 @@ def create_main_layout():
             'background-position': 'center center',
             'position': 'relative',
         } ),
-        html.Div(["Третий фон"
+
+#3 слайд
+        html.Div([ html.H1('Следить за курсом, чтобы', className='main-layout-header-text-2',
+                           style={'position': 'absolute',
+                                    'top': '15%',
+                                    'left': '10%'}),
+        html.Div([
+            html.Div([
+                html.Img(src='/assets/img1.png',
+                         style={'width': '4vw',
+                                'height': '4vw',
+                                'margin-left': '75px'
+                                }),
+                html.P(['знать все актуальные события',
+                       html.Br(),
+                'и новости мира'],
+                       style={'text-align': 'center', 'margin-top': '20px'}),
+            ], style={'display': 'inline-block',
+                      'position': 'absolute',
+                      'vertical-align': 'top',
+                       'top': '30%',
+                       'left': '20%'}),
+            html.Div([
+                html.Img(src='/assets/img2.png', style={'width': '3.5vw', 'height': '4vw','margin-left': '55px'}),
+                html.P(['регулировать свои',
+                       html.Br(),
+                       'сбережения'],
+                       style={'text-align': 'center', 'margin-top': '20px'}),
+            ], style={'display': 'inline-block',
+                      'position': 'absolute',
+                       'top': '30%',
+                       'left': '48%'}),
+            html.Div([
+                html.Img(src='/assets/img3.png', style={'width': '4vw', 'height': '4vw','margin-left': '65px'}),
+                html.P(['создавать свою стратегию',
+                       html.Br(),
+                       'инвестирования'],
+                       style={'text-align': 'center', 'margin-top': '20px'}),
+            ], style={'display': 'inline-block',
+                      'position': 'absolute',
+                       'top': '30%',
+                       'left': '70%'}),
+        ]),
+        html.H1('А также вы можете', className='main-layout-header-text-2',
+                           style={'position': 'absolute',
+                                    'top': '55%',
+                                    'left': '10%'}),
+        html.Div([
+            html.Div([
+                html.Img(src='/assets/img4.png', style={'width': '4vw', 'height': '4vw','margin-left': '70px'}),
+                html.P(['прогнозировать изменения',
+                       html.Br(),
+                       'курса рубля'],
+                       style={'text-align': 'center', 'margin-top': '20px'}),
+            ], style={'display': 'inline-block',
+                      'position': 'absolute',
+                       'top': '70%',
+                       'left': '20%'}),
+            html.Div([
+                html.Img(src='/assets/img5.png', style={'width': '4vw', 'height': '4vw','margin-left': '75px'}),
+                html.P(['оценивать  экономическое',
+                       html.Br(),
+                       'положение России в текущий',
+                       html.Br(),
+                       'момент и перспективы'],
+                       style={'text-align': 'center', 'margin-top': '20px'}),
+            ], style={'display': 'inline-block',
+                      'position': 'absolute',
+                       'top': '70%',
+                       'left': '45%'}),
+            html.Div([
+                html.Img(src='/assets/img6.png', style={'width': '7.5vw', 'height': '4vw','margin-left': '30px'}),
+                html.P(['анализировать текущую',
+                       html.Br(),
+                       ' обстановку на рынке'],
+                       style={'text-align': 'center', 'margin-top': '20px'}),
+            ], style={'display': 'inline-block',
+                      'position': 'absolute',
+                       'top': '70%',
+                       'left': '70%'}),
+        ]),
                   ], style={
-            'background-image': 'url(assets/woman.jpg)',
+            'background-image': 'url(assets/woman-light.png)',
             'height': '90vh',
             'background-size': 'cover',
             'background-position': 'center center',
             'position': 'relative',
         }),
-        html.Div(["Четвертый фон"
+
+#4 слайд
+        html.Div([html.H1('Заголовок слайда', style={}),
+        html.Div([
+            html.P('Первая строка'),
+            html.P('Вторая строка'),
+            html.P('Третья строка'),
+        ], style={})
                   ], style={
             'background-color': 'rgba(245, 245, 245, 1)',
             'height': '90vh',
@@ -61,7 +207,39 @@ def create_main_layout():
             'background-position': 'center center',
             'position': 'relative',
         }),
-        html.Div(["Пятый фон"
+
+
+#5 слайд
+        html.Div([
+            html.Div([
+                html.H1('Будь в Курсе', className='main-layout-header-text', style={
+                    'position': 'relative',
+                    'top': '50%',
+                    'left': '50%',
+                    'transform': 'translate(-50%, 0%)',
+                    'text-align': 'center',
+                    'color': 'white',
+                }),
+                html.Div([
+                    html.P(['вместе с нами'],
+                           className='main-layout-text', style={'font-size': '2vw', 'text-align':'center'}),
+                    html.A(html.Button([
+                        html.Span('Быть в курсе', className='follow-text'),
+                    ], id='follow-currency-button', className='follow-currency-button',
+                        style={'height': '3vw', 'width': '15vw', 'display': 'flex', 'align-items': 'center',
+                               'justify-content': 'center',
+                               'margin-left': 'auto', 'margin-right': 'auto'}),
+                        href='/currency-analysis', style={'text-decoration': 'none'}),
+                ]),
+            ], style={
+                'position': 'absolute',
+                'top': '50%',
+                'left': '50%',
+                'transform': 'translate(-50%, -50%)',
+                'text-align': 'center',
+                'color': 'white',
+            }),
+
                   ], style={
             'background-image': 'url(assets/notebook.jpg)',
             'height': '90vh',
