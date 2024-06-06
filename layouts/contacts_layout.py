2196106300ac23aaeb57e889e8343e632a307783
bryzgalova <eibryzgalova@mai.education>
2024-06-06 23:30:24 +0300

    готов 1 слайд контактов, создана и добавлена ссылка на гугл форму

--- layouts/contacts_layout.py
+++ layouts/contacts_layout.py
@@ -2,44 +2,60 @@ from dash import html, dcc
 
 def create_contacts_layout():
     layout = html.Div([
-        html.H1(html.Strong("Контакты"), className="text-center mt-5"),
-        html.Br(),
-        html.P(html.Strong('Мы всегда открыты к новым предложениям и сотрудничеству.  Наша команда стремится сделать для вас лучший сервис по анализу новостей и событий. Поэтому будем рады услышать ваши честные отзывы!')),
-        #html.Br(),
-        html.P(html.Strong('Вы можете связаться с нами:'), className="text-center mt-5"),
-        html.P(html.Strong('Email:'), style={'margin-left': '200px','display': 'inline-block', 'width': '60px'}),
-        html.P('teamCIFRA@gmail.com', style={'display': 'inline-block', 'margin-right': '350px'}),
-        html.P(html.Strong('Tg:'), style={'display': 'inline-block', 'width': '50px'}),
-        html.P('@teamCIFRA', style={'display': 'inline-block'}),
-        html.Br(),
-        html.Br(),
-        html.Br(),
-        html.P(html.Strong('Наша команда')),
-        html.Br(),
+        # 1 слайд
         html.Div([
-            # Первый столбец
             html.Div([
-                html.Img(src='/assets/avatar.png', style={'width': '10%', 'height': '10%'}),
-                html.Br(),
-                html.Br(),
-                html.Img(src='/assets/avatar.png', style={'width': '10%', 'height': '10%'}),
-                html.Br(),
-                html.Br(),
-                html.Img(src='/assets/avatar.png', style={'width': '10%', 'height': '10%'}),
-                html.Br(),
-                html.Br(),
-                html.Img(src='/assets/avatar.png', style={'width': '10%', 'height': '10%'}),
-            ], style={'width': '25%', 'height': '100%','float': 'left'}),
-
-            html.Div([
-            html.P('Имя 1', className="text"),
-            html.P('Имя 2',className="text"),
-            html.P('Имя 3', className="text"),
-            html.P('Имя 4', className="text"),
-                ], className="block-html"),
-
-
-        ])
+                html.Div([
+                    html.H1([
+                        'Наша команда стремится создать для вас лучший',
+                        html.Br(),
+                        'сервис по анализу курса рубля'
+                    ], className='main-layout-header-text-2', style={
+                        'text-align': 'left',
+                        'margin-top': '8vw',
+                        'color': 'black',
+                    }),
+                    html.P('Будем рады услышать ваши честные отзывы!',
+                           className='main-layout-text', style={
+                               'text-align': 'left',
+                               'font-size': '2vw',
+                               'margin-top': '2vw'  # отступ сверху, чтобы текст не был вплотную к заголовку
+                           }),
+                    html.A(html.Button([
+                        html.Span('Оставить отзыв', className='follow-text'),
+                    ], id='follow-currency-button', className='follow-currency-button',
+                        style={
+                            'height': '3vw',
+                            'width': '15vw',
+                            'display': 'flex',
+                            'align-items': 'center',
+                            'justify-content': 'center',
+                            'margin-top': '2vw',  # отступ сверху от текста
+                        }),
+                        href='https://forms.gle/46Y8vvtya9eCse6F6', style={'text-decoration': 'none'}),
+                ], style={
+                    'display': 'flex',
+                    'flex-direction': 'column',
+                    'align-items': 'flex-start',  # выравнивание по левому краю
+                }),
+                html.Img(src='/assets/Vectorcontacts1.png', style={
+                    'height': '22vw',  # высота изображения, измените по необходимости
+                    'margin-left': '0.4vw',  # отступ слева от изображения
+                    'margin-top': '13vw',
+                }),
+            ], style={
+                'display': 'flex',
+                'align-items': 'flex-start',  # выравнивание по левому краю внутри блока
+                'margin-left': '5vw',  # отступ слева от внешнего блока
+                'margin-top': '0vw',  # отступ сверху от внешнего блока
+                'color': 'black',
+            }),
+        ], style={
+            'background-color': 'rgba(245, 245, 245, 1)',
+            'height': '90vh',
+            'background-size': 'cover',
+            'background-position': 'center center',
+            'position': 'relative',
+        }),
     ])
-
     return layout
