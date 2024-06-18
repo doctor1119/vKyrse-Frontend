a08b427c5264a000c2b0133fd38f5fa032836de6
bryzgalova <eibryzgalova@mai.education>
2024-06-18 12:04:33 +0300

    добавлена кликабельность кнопки

--- layouts/analytics_layout.py
+++ layouts/analytics_layout.py
@@ -7,11 +7,11 @@ def create_analytics_layout():
         # 1 слайд
         html.Div([
             html.Div([
+                html.Div([
                 html.H1("ТЕКУЩИЙ КУРС ВАЛЮТЫ", className='main-layout-header-text-2', style={'text-align': 'left', 'margin-top': '3%'}),
                 html.Div([
                     html.Img(src='/assets/bookmarks.png', style={'height': '3vw', 'margin-right': '0.5vw'}),
-                    html.H2("На этом графике можно посмотреть, как меняется курс рубля по отношению к доллару за конкретный промежуток времени.",
-                            className='text', style={'display': 'inline', 'vertical-align': 'middle', 'text-align': 'left'}),
+                    html.H2("На этом графике можно посмотреть, как меняется курс рубля по отношению к доллару за конкретный промежуток времени.", className='text', style={'display': 'inline', 'vertical-align': 'middle', 'text-align': 'left'}),
                 ], style={'display': 'flex', 'justify-content': 'flex-start', 'align-items': 'center', 'margin-top': '1%'}),
             ], style={'margin-left': '3%', 'width': '50%'}),
             html.Div([
@@ -19,6 +19,7 @@ def create_analytics_layout():
                 html.Img(src='/assets/attach_money.png',
                          style={'height': '26vw', 'position': 'relative', 'margin-left': '70%', 'margin-top': '-28%'}),
             ], style={'position': 'relative', 'width': '100%', 'height': '0'}),
+            ], style={'position': 'relative', 'z-index': '-1'}),
             html.Div([
                 html.Img(src='/assets/calendar_month.png', style={'height': '2vw', 'margin-right': '1vw'}),
                 dcc.DatePickerRange(
@@ -31,7 +32,7 @@ def create_analytics_layout():
                     'background-color': '#30D9CA',
                     'border-color': '#30D9CA',
                     'margin-left': '1vw',
-                    'z-index': '10'
+                    'z-index': '0'
                 }),
             ], style={
                 'display': 'flex',
@@ -47,12 +48,13 @@ def create_analytics_layout():
                         dcc.Graph(id='graph-1', figure=go.Figure(), style={'height': '50vh'}),
                     ])
                 ]), width=6, style={'margin-left': '3%'}),
-            ], className='mb-4', style={'z-index': '-1', 'margin-top': '1%'}),
-        ], style={'height': '90vh', 'position': 'relative', 'z-index': '-1'}),
+            ], className='mb-4', style={'margin-top': '1%', 'position': 'relative', 'z-index': '-1'}),
+        ], style={'height': '90vh', 'position': 'relative'}),
 
         # 2 слайд
         html.Div([
             html.Div([
+                html.Div([
                 html.H1("КУРС ВАЛЮТЫ ПО СОБЫТИЯМ", className='main-layout-header-text-2', style={'text-align': 'left', 'margin-top': '3%'}),
                 html.Div([
                     html.Img(src='/assets/bookmarks.png', style={'height': '3vw', 'margin-right': '0.5vw'}),
@@ -64,21 +66,36 @@ def create_analytics_layout():
                 html.Img(src='/assets/attach_money.png',
                          style={'height': '26vw', 'position': 'relative', 'margin-left': '70%', 'margin-top': '-28%'}),
             ], style={'position': 'relative', 'width': '100%', 'height': '0'}),
+            ], style={'position': 'relative', 'z-index': '-1'}),
             html.Div([
-                html.Img(src='/assets/calendar_month.png', style={'height': '2vw', 'margin-right': '1vw'}),
+                html.Img(src='/assets/calendar_month.png', style={'height': '2vw', 'margin-right': '1vw', 'z-index': '-1'}),
                 dcc.DatePickerRange(
                     id='date-picker-range-2',
                     start_date_placeholder_text="Начало",
                     end_date_placeholder_text="Конец",
                     calendar_orientation='horizontal',
                 ),
-                html.Button('Загрузить данные', id='load-data-button-2', n_clicks=0, className='btn btn-primary ml-3', style={
-                    'background-color': '#30D9CA',
-                    'border-color': '#30D9CA',
-                    'margin-left': '1vw',
-                    'position': 'relative',
-                    'z-index': '10',
-                }),
+                # html.Button('Загрузить данные', id='load-data-button-2', n_clicks=0, className='btn btn-primary ml-3', style={
+                #     'background-color': '#30D9CA',
+                #     'border-color': '#30D9CA',
+                #     'margin-left': '1vw',
+                #     'position': 'relative',
+                #     'z-index': '-1',
+                # }),
+                html.Div([
+                    html.A(html.Button([
+                        html.Span('Загрузить данные', className='load-data-button'),
+                    ], id='load-data-button', className='btn btn-primary ml-3',
+                        style={'height': 'auto', 'width': 'auto', 'display': 'flex', 'align-items': 'center',
+                               'background-color': '#30D9CA',
+                               'border-color': '#30D9CA',
+                               'margin-left': '1vw',
+                               'justify-content': 'center',
+                               'margin-right': 'auto',
+                               'z-index': '-1',
+                               }),
+                        href='/currency-analysis', style={'text-decoration': 'none'}),
+                ]),
             ], style={
                 'display': 'flex',
                 'align-items': 'center',
@@ -89,16 +106,17 @@ def create_analytics_layout():
             dbc.Row([
                 dbc.Col(dbc.Card([
                     dbc.CardBody([
-                        html.H4("График 1", className="card-title"),
+                        html.H4("График 2", className="card-title"),
                         dcc.Graph(id='graph-1', figure=go.Figure(), style={'height': '50vh'}),
                     ])
                 ]), width=6, style={'margin-left': '3%'}),
-            ], className='mb-4', style={'z-index': '-1', 'margin-top': '1%', 'position': 'relative'}),
+            ], className='mb-4', style={'margin-top': '1%', 'position': 'relative', 'z-index': '-1'}),
         ], style={'height': '90vh', 'position': 'relative'}),
 
         # 3 слайд
         html.Div([
             html.Div([
+                html.Div([
                 html.H1("КОЭФФИЦИЕНТ ВЛИЯНИЯ НОВОСТЕЙ", className='main-layout-header-text-2', style={'text-align': 'left', 'margin-top': '3%'}),
                 html.Div([
                     html.Img(src='/assets/bookmarks.png', style={'height': '3vw', 'margin-right': '0.5vw'}),
@@ -110,6 +128,8 @@ def create_analytics_layout():
                 html.Img(src='/assets/attach_money.png',
                          style={'height': '26vw', 'position': 'relative', 'margin-left': '70%', 'margin-top': '-28%'}),
             ], style={'position': 'relative', 'width': '100%', 'height': '0'}),
+            ], style={'position': 'relative', 'z-index': '-1'}),
+
             html.Div([
                 html.Img(src='/assets/calendar_month.png', style={'height': '2vw', 'margin-right': '1vw'}),
                 dcc.DatePickerRange(
@@ -132,13 +152,13 @@ def create_analytics_layout():
             dbc.Row([
                 dbc.Col(dbc.Card([
                     dbc.CardBody([
-                        html.H4("столбчатая диаграмма", className="card-title"),
+                        html.H4("Cтолбчатая диаграмма", className="card-title"),
                         dcc.Graph(id='bar-chart', figure=go.Figure(
                             go.Bar(x=['Category A', 'Category B', 'Category C'], y=[10, 20, 30])), style={'height': '50vh'}),
                     ])
                 ]), width=6, style={'margin-left': '3%'}),
-            ], className='mb-4', style={'z-index': '-1', 'margin-top': '1%'}),
-        ], style={'height': '90vh', 'position': 'relative', 'z-index': '-1'}),
+            ], className='mb-4', style={'margin-top': '1%', 'position': 'relative', 'z-index': '-1'}),
+        ], style={'height': '90vh', 'position': 'relative'}),
 
         # подвал
         html.Div([
@@ -167,5 +187,5 @@ def create_analytics_layout():
                 'position': 'relative',
                 'z-index': '-1',
             }),
-    ], style={ 'z-index': '-1'})
+    ], style={'z-index': '-1'})
     return layout
